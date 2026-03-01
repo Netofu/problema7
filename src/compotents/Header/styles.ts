bc96ea307bb0f0d0c279aaeff81ad2f222abfe68
Netofu <98113246+Netofu@users.noreply.github.com>
2026-03-01 20:56:51 -0300

    Add styled components for Header styles

---
+++ src/compotents/Header/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background-color: #2c3e50;
+  color: white;
+  padding: 20px;
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 1rem;
+  opacity: 0.8;
+  margin-top: 10px;
+`;
